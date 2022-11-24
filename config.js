0e2538caf539adc59ff0009ea771c00ac8eda78b
dangon200 <gonzalezdaniel230@gmail.com>
2022-11-24 02:18:14 -0300

    asd

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
-export const PORT = process.env.PORT
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USER || 'postgres'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '12345678'
-export const DB_NAME = process.env.DB_NAME || 'videogames'
-export const DB_PORT = process.env.DB_PORT || 'localhost'
-export const API_KEY = process.env.DB_PORT || '569dc6e763c54ca9ac9516b8286d4171'
+const PORT = process.env.PORT
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_USER = process.env.DB_USER || 'postgres'
+const DB_PASSWORD = process.env.DB_PASSWORD || '12345678'
+const DB_NAME = process.env.DB_NAME || 'videogames' 
+const DB_PORT = process.env.DB_PORT || 'localhost'
+const API_KEY = process.env.DB_PORT || '569dc6e763c54ca9ac9516b8286d4171'
+
+
+module.exports = { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, API_KEY }
